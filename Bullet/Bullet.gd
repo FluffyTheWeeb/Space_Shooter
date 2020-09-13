55988a41631db0c3ad7cd5741436fde226392385
FluffyTheWeeb <70995044+FluffyTheWeeb@users.noreply.github.com>
2020-09-13 06:19:00 -0600

    Movement, animation and bullets!
    Animacion agregada!
    balitas agregadas!

---
+++ Bullet/Bullet.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+export var speed: float = 500
+
+func _physics_process(delta):
+	position.y -= speed * delta
+	
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,12 +1,35 @@
 extends Area2D
 
+var plBullet := preload("res://Bullet/Bullet.tscn")
+
+onready var animatedSprite := $AnimatedSprite
+onready var firingPositions := $FiringPositions
+onready var fireDelayTime := $FireDelayTime
 
 export var speed: float = 100
+export var fireDelay: float = 0.1
 var vel:= Vector2(0, 0)
 
+
 func _process(delta):
-	pass
+	# Animate
+	if vel.x < 0:
+		animatedSprite.play("left")
+	elif vel.x > 0:
+		animatedSprite.play("right")
+	else:
+		animatedSprite.play("straight")
+	#Checks if shooting
+	if Input.is_action_pressed("shoot") and fireDelayTime.is_stopped():
+		fireDelayTime.start(fireDelay)
+		for child in firingPositions.get_children():
+			var bullet := plBullet.instance()
+			bullet.global_position = child.global_position
+			get_tree().current_scene.add_child(bullet)
+
+
 func _physics_process(delta):
+	#Movement
 	var dirVec := Vector2(0, 0)
 	
 	if Input.is_action_pressed("move_left"):
